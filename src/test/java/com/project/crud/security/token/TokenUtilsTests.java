98ee4d4b1c87eb36a8d375cb37c2e068c3557366
choijaewon323 <casdfghjke@naver.com>
2024-11-16 15:44:28 +0900

    refactor: 테스트 클래스에 public 제거

--- src/test/java/com/project/crud/security/token/TokenUtilsTests.java
+++ src/test/java/com/project/crud/security/token/TokenUtilsTests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TokenUtilsTests {
+class TokenUtilsTests {
 
     UserTokenResponse userToken;
 
