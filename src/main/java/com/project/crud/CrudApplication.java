e244a052a70898899209ec07968afb35d21c0df8
choijaewon323 <casdfghjke@naver.com>
2024-11-16 15:43:50 +0900

    feat: 초기 값 추가

--- src/main/java/com/project/crud/CrudApplication.java
+++ src/main/java/com/project/crud/CrudApplication.java
@@ -1,15 +1,32 @@
 package com.project.crud;
 
+import com.project.crud.board.domain.Board;
+import com.project.crud.board.repository.BoardRepository;
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.stream.Stream;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class CrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(CrudApplication.class, args);
+
+		BoardRepository boardRepository = context.getBean(BoardRepository.class);
+
+		Stream.iterate(1, i -> i + 1)
+				.map(i -> Board.builder()
+						.title("title" + i)
+						.content("content" + i)
+						.writer("writer" + i)
+						.build())
+				.limit(50)
+				.forEach(boardRepository::save);
 	}
 
 }
