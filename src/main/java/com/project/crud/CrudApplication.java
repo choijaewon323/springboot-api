2c22b7f25db2e848ed04f15e78ca80085d193d89
choijaewon323 <casdfghjke@naver.com>
2024-12-07 18:18:32 +0900

    feat: 컨트롤러 실행 시간 ms aop 적용

--- src/main/java/com/project/crud/CrudApplication.java
+++ src/main/java/com/project/crud/CrudApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.stream.Stream;
 
+@EnableAspectJAutoProxy
 @EnableJpaAuditing
 @SpringBootApplication
 public class CrudApplication {
