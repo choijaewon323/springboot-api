a09b9c277e0d99bcfbbfe300ca22cc97fd23dbf5
choijaewon323 <casdfghjke@naver.com>
2024-12-18 16:17:09 +0900

    feat: 세션 생성 로직 추가

--- src/main/java/com/project/crud/login/custom/CustomSuccessHandler.java
+++ src/main/java/com/project/crud/login/custom/CustomSuccessHandler.java
@@ -5,31 +5,33 @@ import com.project.crud.login.dto.LoginResponseDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 import java.io.IOException;
 
 @RequiredArgsConstructor
-public class CustomSuccessHandler implements AuthenticationSuccessHandler {
+public class CustomSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-        System.out.println(auth.getAuthorities());
-
-        System.out.println(auth.getName());
-
         response.setContentType("application/json; charset=utf-8");
-
         LoginResponseDto responseDto = new LoginResponseDto(true, "로그인 성공");
 
         String content = objectMapper.writeValueAsString(responseDto);
+
+        SecurityContext context = SecurityContextHolder.getContext();
+
+        HttpSession httpSession = request.getSession(true);
+        httpSession.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, context);
 
         response.getWriter().write(content);
     }
